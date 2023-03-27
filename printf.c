966d6e31e225f9a9204c8c433f08a1a05fb551d0
alemao51092 <aarielmartinez1111@gmail.com>
2023-03-27 09:20:46 -0700

    opciones

---
+++ printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0;
+
+	va_start(args, format);
+	while (format && format[i])
+		i++;
+	opciones[j].function[args];
