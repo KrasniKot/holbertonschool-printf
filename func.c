12b656889f1eed8f71be9117dd12d243f33212e5
KrasniKot <6187@holbertonstudents.com>
2023-03-27 17:14:56 -0700

    stringAndPercentWorking

--- func.c
+++ func.c
@@ -7,7 +7,15 @@ void character(va_list arg)
 
 void string(va_list arg)
 {
+	char *s = va_arg(arg, char *);
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		pch(s[i]);
+	}
 }
 void percent(va_list arg)
 {
+	pch(37);
 }
--- main.c
+++ main.c
@@ -4,6 +4,10 @@ int main(void)
 {
 	_printf("character: %c", 'c');
 	putchar (10);
+	_printf("string is a: %s", "string");
+	putchar (10);
+	_printf("percent is a: %%");
+	putchar (10);
 
 	return (0);
 }
